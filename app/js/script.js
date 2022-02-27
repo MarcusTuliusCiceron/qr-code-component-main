06ddcd70dc40ee24591f6e53a877433f5febb161
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-02-27 11:16:08 +0100

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,29 @@
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body')
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger')
+    if (header.classList.contains('open')){
+        header.classList.remove('open')
+        body.classList.remove('noscroll')
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-out')
+            element.classList.remove('fade-in')
+        })
+        
+
+    }
+    else{
+        header.classList.add('open')
+        body.classList.add('noscroll')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
+        
+    }
+    
+})
